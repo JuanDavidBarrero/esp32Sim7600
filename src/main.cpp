8bc276503193c7839a7c879d615b276872096059
JuanDavidBarrero <Juandbarrero@hotmail.com>
2025-04-16 16:59:42 -0500

    ejemplo de mqtts agregado

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include "FS.h"
+#include "SPIFFS.h"
+
 #include "core.h"
 #include "config.h"
 #include "gps.h"
@@ -15,9 +18,41 @@ CelularModule cellularModule(BAUD_RATE, RX_PIN, TX_PIN);
 ConfigModule config(cellularModule);
 // SmsClient smsClient(cellularModule);
 // SslConfig sslClient(cellularModule);
-GPSModule gpsClient(cellularModule);
+// GPSModule gpsClient(cellularModule);
 // HttpClientModule httpclient(cellularModule);
 // MqttClient mqttClient(cellularModule);
+
+bool loadAndSendCert(const char *path, const char *name)
+{
+  File file = SPIFFS.open(path);
+  if (!file || file.isDirectory())
+  {
+    Serial.print("No se pudo abrir ");
+    Serial.print(path);
+    Serial.println(" para lectura");
+    return false;
+  }
+
+  const size_t MAX_CERT_SIZE = 4096;
+  char buffer[MAX_CERT_SIZE];
+
+  size_t i = 0;
+  while (file.available() && i < MAX_CERT_SIZE - 1)
+  {
+    buffer[i++] = file.read();
+  }
+  buffer[i] = '\0'; 
+
+  file.close();
+
+  // Serial.print("Contenido de ");
+  // Serial.println(path);
+  // Serial.println(buffer);  // imprimir todo el contenido
+
+  // sslClient.downloadCert(name, (const uint8_t *)buffer, i);
+
+  return true;
+}
 
 void setup()
 {
@@ -36,44 +71,39 @@ void setup()
   delay(5000);
 
 
-  Serial.println("\n\n\n=============================  GPS EXAMPLE =============================\r\n\n");
 
+  // Serial.println("\n\n\n=============================  GPS EXAMPLE =============================\r\n\n");
 
-  gpsClient.activeGPS();
+  // gpsClient.activeGPS();
 
-  if(gpsClient.isActiveGPS()){
+  // if(gpsClient.isActiveGPS()){
 
-    Coordinates dataGPS;
-    Serial.println("\r\nEl gps esta activo\r\n");
+  //   Coordinates dataGPS;
+  //   Serial.println("\r\nEl gps esta activo\r\n");
 
-    while (!gpsClient.getGPS())
-    {
-      delay(1000);
-    }
+  //   while (!gpsClient.getGPS())
+  //   {
+  //     delay(1000);
+  //   }
 
-    dataGPS = gpsClient.getCoordinates();
-    Serial.println(dataGPS.latitude);
-    Serial.println(dataGPS.longitude);
-    
+  //   dataGPS = gpsClient.getCoordinates();
+  //   Serial.println(dataGPS.latitude);
+  //   Serial.println(dataGPS.longitude);
 
-  }
+  // }
 
-  gpsClient.deactivateGPS();
+  // gpsClient.deactivateGPS();
 
-
-  Serial.println("\n\n\n=============================  END GPS EXAMPLE =============================\r\n\n");
-
-
+  // Serial.println("\n\n\n=============================  END GPS EXAMPLE =============================\r\n\n");
 
   // Serial.println("\n\n\n=============================  SMS EXAMPLE =============================\r\n\n");
 
-  
   // smsClient.listSMS();
 
   // smsClient.readSMS(1);
 
   // smsClient.deleteSMS(0);
-  
+
   // smsClient.deleteAllSMS();
   // smsClient.sendSMS("+57XXXXXXXXX","Hola desde el esp32");
 
@@ -100,10 +130,31 @@ void setup()
 
   // Serial.println("\n\n\n=============================  MQTT EXAMPLE =============================\r\n\n");
 
+  // Serial.println("Cargando certificados");
+
+  //   // Inicializar SPIFFS
+  //   if (!SPIFFS.begin(true))
+  //   {
+  //     Serial.println("Error al montar SPIFFS");
+  //     return;
+  //   }
+  
+
+  // loadAndSendCert("/cacert.pem", "cacert.pem");
+  // loadAndSendCert("/cert.crt", "cert.crt");
+  // loadAndSendCert("/priv.key", "priv.pem");
+
+  // sslClient.deleteCert("client_cert.pem");
+  // sslClient.deleteCert("cert.crt");
+  // sslClient.deleteCert("priv.key");
+
+  // sslClient.activateServerandClient("cacert.pem", "cert.crt", "priv.pem");  // only for tls
+
   // mqttClient.startMqtt();
   // mqttClient.setBrokerUrl("broker.emqx.io");
-  // mqttClient.setPort(1883);
-  // mqttClient.setClient("esp-client",false);
+  // mqttClient.setPort(1883); // change por if use tls
+  // mqttClient.setClient("esp-client", false);
+  // // mqttClient.setClient("esp-client", true); // only if use mqtts
   // mqttClient.setLastWillTopic("test/lastwill");
   // mqttClient.setLastWillMsg("I'm offline!");
 
@@ -115,7 +166,7 @@ void setup()
 
   //   delay(1000);
 
-  //   mqttClient.publish("test/topic/juan28","{\"msg\":\"hello hades\"}");
+  //   mqttClient.publish("gps/data/test/", "{\"lat\":4.60971,\"lon\":-74.08175,\"vel\":15.3,\"ts\":\"2025-04-16T14:30:00Z\"}");
 
   //   delay(1000);
 
@@ -123,14 +174,15 @@ void setup()
   //   while (1)
   //   {
   //     mqttClient.readMsg();
-  //     mqttClient.publish("test/topic/juan28","{\"msg\":\"hello hades\"}");
-  //     if(count > 10){
+  //     mqttClient.publish("gps/data/test/", "{\"lat\":4.60971,\"lon\":-74.08175,\"vel\":15.3,\"ts\":\"2025-04-16T14:30:00Z\"}");
+
+  //     if (count > 10)
+  //     {
   //       break;
   //     }
   //     count++;
   //     delay(5000);
   //   }
-
   // }
   // else
   // {
